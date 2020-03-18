6ad1f563e719fe6628f335e0bf5a67aa0a38b006
zubayrrr <abdulfaiz076@gmail.com>
2020-03-18 19:52:08 +0530

    first commit and last commit

---
+++ client/index.js
@@ -0,0 +1,125 @@
+import Web3 from "web3";
+import Crud from "../build/contracts/Crud.json";
+
+let web3;
+let crud;
+
+const initWeb3 = () => {
+  return new Promise((resolve, reject) => {
+    if (typeof window.ethereum !== "undefined") {
+      const web3 = new Web3(window.ethereum);
+      window.ethereum
+        .enable()
+        .then(() => {
+          resolve(new Web3(window.ethereum));
+        })
+        .catch(e => {
+          reject(e);
+        });
+      return;
+    }
+    if (typeof window.web3 !== "undefined") {
+      return resolve(new Web3(window.web3.currentProvider));
+    }
+    resolve(new Web3("http://localhost:9545"));
+  });
+};
+
+const initContract = () => {
+  const deploymentKey = Object.keys(Crud.networks)[0];
+  return new web3.eth.Contract(Crud.abi, Crud.networks[deploymentKey].address);
+};
+
+const initApp = () => {
+  const $create = document.getElementById("create");
+  const $createResult = document.getElementById("create-result");
+
+  const $read = document.getElementById("read");
+  const $readResult = document.getElementById("read-result");
+
+  const $edit = document.getElementById("edit");
+  const $editResult = document.getElementById("edit-result");
+
+  const $delete = document.getElementById("delete");
+  const $deleteResult = document.getElementById("delete-result");
+
+  let accounts = [];
+
+  web3.eth.getAccounts().then(_accounts => {
+    accounts = _accounts;
+  });
+
+  // create  event
+
+  $create.addEventListener("submit", e => {
+    e.preventDefault();
+    const name = e.target.elements[0].value;
+    crud.methods
+      .create(name)
+      .send({ from: accounts[0] })
+      .then(() => {
+        $createResult.innerHTML = `New user created: ${name}`;
+      })
+      .catch(() => {
+        $createResult.innerHTML = `Oops! There was an error while trying to create a new user.`;
+      });
+  });
+
+  // read event
+  $read.addEventListener("submit", e => {
+    e.preventDefault();
+    const id = e.target.elements[0].value;
+    crud.methods
+      .read(id)
+      .call()
+      .then(result => {
+        $readResult.innerHTML = `Id: ${result[0]} Name: ${result[1]}`;
+      })
+      .catch(() => {
+        $readResult.innerHTML = `Oops! There was an error while fetching the user.`;
+      });
+  });
+
+  // edit event
+
+  $edit.addEventListener("submit", e => {
+    e.preventDefault();
+    const id = e.target.elements[0].value;
+    const name = e.target.elements[1].value;
+    crud.methods
+      .update(id, name)
+      .send({ from: accounts[0] })
+      .then(result => {
+        $editResult.innerHTML = `The user has been updated, ${id} is now ${name}`;
+      })
+      .catch(() => {
+        $editResult.innerHTML = `Oops there was an error while trying to update the user ${id}`;
+      });
+  });
+
+  // delet event
+
+  $delete.addEventListener("submit", e => {
+    e.preventDefault();
+    const id = e.target.elements[0].value;
+    crud.methods
+      .destroy(id)
+      .send({ from: accounts[0] })
+      .then(result => {
+        $deleteResult.innerHTML = `${id} has been deleted successfully!`;
+      })
+      .catch(() => {
+        $deleteResult.innerHTML = `Oops! There was an error while deleting ${id}`;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  initWeb3()
+    .then(_web3 => {
+      web3 = _web3;
+      crud = initContract();
+      initApp();
+    })
+    .catch(e => console.log(e.message));
+});
